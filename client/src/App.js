8399b6a4cdc8dafc6b69e43974bf64552550c42a
Roman Viktorovich <roman.viktorovich@outlook.com>
2023-01-06 12:32:51 +0300

    refactor

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import {Main} from "./components/Main/Main";
+import {Header} from "./components/Header/Header";
+
+function App() {
+    return (
+        <>
+                <Header/>
+                <Main/>
+        </>
+    );
+}
+export default App;
