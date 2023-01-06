8b2580e39cf21e2fd73626b8d46f9bfac69c11d9
Roman Viktorovich <roman.viktorovich@outlook.com>
2023-01-06 19:33:38 +0300

    work app

--- client/src/ui/Main/Main.jsx
+++ client/src/ui/Main/Main.jsx
@@ -1,20 +1,37 @@
-import s from "./Main.module.css";
-import { Ul } from "..//..//components/Ul/Ul";
-import { Button } from "../../components/Button/Button";
+import s from "./Main.module.css"
+import { Ul } from "../../components/Ul/Ul"
+import { Button } from "../../components/Button/Button"
 import { Sliders } from "../../components/Sliders/Sliders";
+import { Card } from "../../components/Card/Card";
+import { NameProduct } from "../../components/NameProduct/NameProduct";
+import { Container } from "../../components/Container/Container";
+import {New} from "../../components/New/New";
 
 export function Main() {
-	return (
-		<main>
-			<section>
-				<div className={s.container}>
-					<div className={s.menu}>
-						<Ul></Ul>
-						<Button></Button>
-					</div>
-				</div>
-			</section>
-			<Sliders />
-		</main>
-	);
+    return (
+        <main>
+    <section>
+        <Container>
+            <div className={s.menu}>
+                <Ul></Ul>
+                <Button></Button>
+            </div>
+        </Container>
+    </section>
+    <section className={s.range}>
+        <div className={s.container}>
+            <Sliders/>
+            <New/>
+            <NameProduct></NameProduct>
+         <div className={s.cards__items}>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+         </div>
+        </div>
+    </section>
+        </main>
+    )
 }
