3af12381b19a19586c9f95428b3ea8c4664bba26
Roman Viktorovich <roman.viktorovich@outlook.com>
2023-01-09 13:58:01 +0300

    ref data

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { New } from "./components/New/New";
 import {Main} from "./Ui/Main/Main";
 import {Header} from "./Ui/Header/Header";
 import {Footer} from "./Ui/Footer/Footer";
-import {Card_2} from "./TestCard/Card_2";
+import {Card_2} from "./TestCard/Card_2"
 
 function App() {
     return (
