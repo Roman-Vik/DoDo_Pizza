d35275103910a625f093f511f0550fe61ca8e30f
Artem <115172694+ArtemEgorov007@users.noreply.github.com>
2023-01-09 18:12:06 +0300

    Slide=>New(creat, mod js)

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
-import { New } from "./components/New/New";
-import {Main} from "./Ui/Main/Main";
-import {Header} from "./Ui/Header/Header";
-import {Footer} from "./Ui/Footer/Footer";
+
+import { Main } from "./Ui/Main/Main";
+import { Header } from "./Ui/Header/Header";
+import { Footer } from "./Ui/Footer/Footer";
 
 function App() {
-    return (
-        <>
-            <Header/>
-
-            <Main/>
-
-            <Footer/>
-        </>
-    );
-
+	return (
+		<>
+			<Header />
+			<Main />
+			<Footer />
+		</>
+	);
 }
 export default App;
--- client/src/components/Sliders/Sliders.jsx
+++ client/src/components/Sliders/Sliders.jsx
@@ -1,28 +1,45 @@
 import s from "./Sliders.module.css";
-import { Container } from ".//..//Container/Container";
+import { Scrollbar } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import "swiper/css/scrollbar";
 
 export function Sliders() {
 	return (
-		<div className={s.sliders}>
-			<Container>
-				<div className={s.carusel}>
+		<>
+			<Swiper
+				modules={[Scrollbar]}
+				spaceBetween={30}
+				slidesPerView={1}
+				navigation
+				pagination={{ clickable: true }}
+				scrollbar={{ draggable: true }}
+				onSwiper={(swiper) => console.log(swiper)}
+				onSlideChange={() => console.log("slide change")}
+			>
+				<SwiperSlide>
 					<img
-						src={require("../images/slider/slide-central.webp")}
-						className={s.sliders__main}
+						src={require("../images/slider/slide-left.png")}
+						className={s.card}
 						alt="slide-central.webp"
 					/>
+				</SwiperSlide>
+				<SwiperSlide>
 					<img
 						src={require("../images/slider/slide-central.webp")}
-						className={s.sliders__main}
+						className={s.card}
 						alt="slide-central.webp"
 					/>
+				</SwiperSlide>
+				<SwiperSlide>
 					<img
-						src={require("../images/slider/slide-central.webp")}
-						className={s.sliders__main}
+						src={require("../images/slider/right-promo.png")}
+						className={s.card}
 						alt="slide-central.webp"
 					/>
-				</div>
-			</Container>
-		</div>
+				</SwiperSlide>
+			</Swiper>
+		</>
 	);
 }
