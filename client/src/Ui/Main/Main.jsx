b79088ef271b39cdecec953caddb4d7e19de8359
Artem <115172694+ArtemEgorov007@users.noreply.github.com>
2023-01-09 18:12:40 +0300

    main

--- client/src/Ui/Main/Main.jsx
+++ client/src/Ui/Main/Main.jsx
@@ -5,31 +5,33 @@ import { Sliders } from "../../components/Sliders/Sliders";
 import { Card } from "../../components/Card/Card";
 import { NameProduct } from "../../components/NameProduct/NameProduct";
 import { Container } from "../../components/Container/Container";
+import { New } from "..//../components/New/New";
 
 export function Main() {
-    return (
-        <main>
-    <section>
-        <Container>
-            <div className={s.menu}>
-                <Ul></Ul>
-                <Button></Button>
-            </div>
-        </Container>
-    </section>
-    <section className={s.range}>
-        <Container>
-            <Sliders/>
-            <NameProduct></NameProduct>
-         <div className={s.cards__items}>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-         </div>
-         </Container>
-    </section>
-        </main>
-    )
+	return (
+		<main>
+			<section>
+				<Container>
+					<div className={s.menu}>
+						<Ul></Ul>
+						<Button></Button>
+					</div>
+				</Container>
+			</section>
+			<section className={s.range}>
+				<Sliders />
+				<Container>
+					<New />
+					<NameProduct></NameProduct>
+					<div className={s.cards__items}>
+						<Card></Card>
+						<Card></Card>
+						<Card></Card>
+						<Card></Card>
+						<Card></Card>
+					</div>
+				</Container>
+			</section>
+		</main>
+	);
 }
