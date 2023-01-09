12a6442ffa186d57faffffb622d9676a029daeb3
Roman Viktorovich <roman.viktorovich@outlook.com>
2023-01-09 19:47:33 +0300

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import { New } from "./components/New/New";
 import {Main} from "./Ui/Main/Main";
-import {Header} from "./Ui/Header/Header";
 import {Footer} from "./Ui/Footer/Footer";
 import {Card_2} from "./TestCard/Card_2";
+import {Header} from "./pages/Header/Header";
 
 function App() {
     return (
         <>
-            <Header/>
+          <Header/>
             <Main/>
             <Footer/>
             <Card_2/>
