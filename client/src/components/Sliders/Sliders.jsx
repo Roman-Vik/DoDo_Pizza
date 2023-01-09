7d8916609227aa3e5d54871cf0e44d018de7da9a
Artem <115172694+ArtemEgorov007@users.noreply.github.com>
2023-01-09 19:02:27 +0300

    Slide=>New(creat, mod js)

--- client/src/components/Sliders/Sliders.jsx
+++ client/src/components/Sliders/Sliders.jsx
@@ -1,18 +1,22 @@
 import s from "./Sliders.module.css";
-import { Scrollbar } from "swiper";
+import { Scrollbar, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
 
 export function Sliders() {
 	return (
 		<>
 			<Swiper
-				modules={[Scrollbar]}
+				modules={[Scrollbar, Autoplay]}
 				spaceBetween={30}
 				slidesPerView={1}
-				navigation
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false
+				}}
 				pagination={{ clickable: true }}
 				scrollbar={{ draggable: true }}
 				onSwiper={(swiper) => console.log(swiper)}
